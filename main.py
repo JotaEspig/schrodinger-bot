9fe0db62a7652623adf21b7797e84246c6387a44
JotaEspig <jotinha1300@gmail.com>
2021-09-28 11:05:14 -0300

    Updates

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
-#Standart Libraries
+# Standart Libraries
 import json
 from os import listdir
 
-#Discord
+# Discord
 import discord
 from discord.ext import commands
 from discord.ext.commands import MissingPermissions
 
 
-#Defines the token and the prefix
+# Defines the token and the prefix
 with open('config.json') as file:
     config = json.load(file)
     TOKEN, PREFIX = config['token'], config['prefix']
 
 
-#Configure the Bot
+# Configure the Bot
 intents = discord.Intents(
-    messages = True,
-    guilds = True,
-    reactions = True,
-    members = True,
-    presences = True
+    messages=True,
+    guilds=True,
+    reactions=True,
+    members=True,
+    presences=True
 )
-client = commands.Bot(command_prefix= PREFIX, intents= intents)
+client = commands.Bot(command_prefix=PREFIX, intents=intents)
+
 
 def is_it_me(ctx) -> bool:
     """Verifica se o autor da mensagem é um dos donos do BOT
@@ -40,7 +41,8 @@ def is_it_me(ctx) -> bool:
     ]
     return ctx.author.id in owners
 
-@client.command(aliases= ['LOAD'])
+
+@client.command(aliases=['LOAD'])
 @commands.check(is_it_me)
 async def load(ctx, extension: str) -> None:
     """Habilita um cog do BOT
@@ -52,7 +54,8 @@ async def load(ctx, extension: str) -> None:
     client.load_extension(f'cogs.{extension}')
     await ctx.send(f'\"{extension}\" carregado')
 
-@client.command(aliases= ['UNLOAD'])
+
+@client.command(aliases=['UNLOAD'])
 @commands.check(is_it_me)
 async def unload(ctx, extension: str) -> None:
     """Desabilita um cog do BOT
@@ -64,7 +67,8 @@ async def unload(ctx, extension: str) -> None:
     client.unload_extension(f'cogs.{extension}')
     await ctx.send(f'\"{extension}\" descarregado')
 
-@client.command(aliases= ['RELOAD'])
+
+@client.command(aliases=['RELOAD'])
 @commands.check(is_it_me)
 async def reload(ctx, extension: str) -> None:
     """Reinicia um cog do BOT
@@ -77,13 +81,14 @@ async def reload(ctx, extension: str) -> None:
     client.load_extension(f'cogs.{extension}')
     await ctx.send(f'\"{extension}\" recarregado')
 
-#Load em todos os cog automaticamente
+# Load em todos os cog automaticamente
 for filename in listdir('./scripts/cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'scripts.cogs.{filename[:-3]}')
 
-#-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X
-#Runs the bot
+
+# -X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X
+# Runs the bot
 @client.event
 async def on_command_error(ctx, error) -> None:
     """Trata dos erros que acontecem durante o funcionamento do BOT
@@ -95,11 +100,12 @@ async def on_command_error(ctx, error) -> None:
     if isinstance(error, MissingPermissions):
         await ctx.send(f':hand_splayed: Você não tem permissão para usar esse comando\n:hand_splayed: {error}') 
 
+
 @client.event
 async def on_ready() -> None:
     """Comandos executados quando o BOT fica online
     """
     print(f'{client.user.name} online')
-    await client.change_presence(activity= discord.Activity(type= discord.ActivityType.listening, name= f'{PREFIX}help'))
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=f'{PREFIX}help'))
 
 client.run(TOKEN)
